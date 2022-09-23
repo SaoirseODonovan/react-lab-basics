b4f2d6be97ddfe4bd383b777dfa70f516766b039
SaoirseODonovan <74667820+SaoirseODonovan@users.noreply.github.com>
2022-09-23 14:19:37 +0100

    React Lab Part 01 Exercise 01.

--- src/components/exercises/01_staticComponent.js
+++ src/components/exercises/01_staticComponent.js
@@ -3,7 +3,29 @@ import "../../../node_modules/bootstrap/dist/css/bootstrap.css";
 
 const Demo = () => {
   return (
-      <h1>TODO</h1>
+  //  <h1>Computer Forensics and Security - Modules table</h1>
+
+    <table className="table table-bordered">
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>No lectures</th>
+        <th>No practicals</th>
+      </tr>
+    </thead>
+    <tbody >
+      <tr>
+        <td>DevOps</td>
+        <td>2</td>
+        <td>2</td>
+      </tr>
+      <tr>
+        <td>Enterprise Web Dev</td>
+        <td>3</td>
+        <td>2</td>
+      </tr>
+    </tbody >
+  </table>
   );
 };
 
